ac69ad63a79d833c9e6c75ceb5a1d6f81d96ab36
Torben Clasen <torbenclasen@gmail.com>
2019-01-28 01:37:36 +0100

    Need to finish the update function

--- src/TicTacToe/State.fs
+++ src/TicTacToe/State.fs
@@ -2,21 +2,41 @@ module TicTacToe.State
 
 open Elmish
 open Types
+open System.Drawing
 
 let initTurn =
   {Number=0; ClickedBy=Clicker.Nobody;ClickedButton=None}
 
+let initButton x y =
+  {
+    Position = Point(x,y);
+    ClickedBy = Clicker.Nobody
+  }
+
 let initBoard =
-  {}
+  let getAllPositions amount =
+    seq { for i in 1 .. amount do
+            for j in 1 .. amount do
+              yield (i , j)
+        }
+
+  let allPositions = getAllPositions 9
+
+  let newButtons = allPositions |> Seq.map (fun x -> initButton (fst x) (snd x)) |> Seq.toList
+
+  {Buttons = newButtons}
+
+let initModel =
+  {GameBoard = initBoard; CurrentTurn = initTurn}
 
 let init () : Model * Cmd<Msg> =
-  0, []
+  (initModel, [])
 
 let update msg model =
   match msg with
-  | Increment ->
+  | PlayersTurn newestTurn ->
       model + 1, []
-  | Decrement ->
+  | ComputerTurn newestTurn ->
       model - 1, []
   | Reset ->
-      0, []
+      (initModel, [])
